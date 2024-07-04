c325bf3b09a2792ed06b2fa973781e0c3776daff
robertyulisman <desainku1092@gmail.com>
2024-07-04 11:37:00 +0700

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://remini-web-self.vercel.app",
+    origin: "https://remini-app-web.vercel.app",
     methods: "GET, POST, PUT, DELETE",
     credentials: true,
   })
